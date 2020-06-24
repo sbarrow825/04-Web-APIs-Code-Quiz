83800c78827b7c1fddb1b188bc54a2adce4b80dc
sbarrow825 <sbarrow825@berkeley.edu>
2020-06-23 23:55:06 -0700

    highscores page now displays with high score from the submitted form

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var secondsLeft = document.querySelector("#seconds-left");
 var startButton = document.querySelector("#start-button");
 var startPage = document.querySelector("#start-page");
 var placeholderDiv = document.querySelector("#place-holder");
+var navbar = document.querySelector("#main-navbar");
 var allButtons;
 var question1Options = ["2", "A string is a datatype", "A string can be mutated", "A string is denoted by quotation marks at the beginning and end", "A string can be saved to local storage on the browser"];
 var question2Options = ["1", "Python", "JavaScript", "CSS", "html"];
@@ -14,6 +15,9 @@ var newBreak;
 var currentQuestion = 0;
 var numberCorrect = 0;
 var numberTotalQuestions = 4;
+var countdownTimer;
+var f;
+var highScoresExist = false;
 // var introTextTitle = document.querySelector("#intro-text-title");
 // var startButtonText = document.querySelector("#start-button-text");
 
@@ -25,14 +29,20 @@ startButton.addEventListener("click", startCountdown);
 
 function startCountdown() {
     setUpQuiz();
-    setInterval(tick, 1000);
+    countdownTimer = setInterval(tick, 1000);
 }
 
 function tick() {
     secondsLeft.innerHTML -= 1;
     if (secondsLeft.innerHTML <= 0) {
         displayEndScreen();
+        stopTimer();
     }
+}
+
+function stopTimer() {
+    clearInterval(countdownTimer);
+    secondsLeft.innerHTML = 0;
 }
 
 function setUpQuiz() {
@@ -143,14 +153,81 @@ function displayEndScreen() {
     newQuestion.appendChild(endScreenMessage);
     addTwoBreaks(newQuestion);
     var yourScore = document.createElement("p");
-    yourScore.innerHTML = "Your final score is " + numberCorrect +" out of " + numberTotalQuestions;
+    yourScore.innerHTML = "Your final score is " + numberCorrect + " out of " + numberTotalQuestions;
     newQuestion.appendChild(yourScore);
     addTwoBreaks(newQuestion);
+
     var enterInitials = document.createElement("p");
-    enterInitials.innerHTML = "Enter Initials:";
+    enterInitials.innerHTML = "Enter your initials:";
     newQuestion.appendChild(enterInitials);
+
+    var f = document.createElement("form");
+    f.setAttribute('action',"highscores.html");
+
+    var i = document.createElement("input"); //input element, text
+    i.setAttribute('type', "text");
+    i.setAttribute('initials', "initials");
+    i.setAttribute('id', 'initials');
+
+    var s = document.createElement("input"); //input element, Submit button
+    s.setAttribute('type', "submit");
+    s.setAttribute('value', "Submit");
+
+    f.appendChild(i);
+    f.appendChild(s);
+
+    console.log(f);
+    f.addEventListener("submit", (e) => {
+        e.preventDefault();
+        var newInitials = (f.elements["initials"].value);
+        if (highScoresExist) {
+            addHighScore(newInitials, numberCorrect, highScoresList);
+        } else {
+            setUpHighscoresPage(newInitials, numberCorrect);
+        }
+    })
+
+    newQuestion.appendChild(f);
     placeholderDiv.appendChild(newQuestion);
 }
+
+function setUpHighscoresPage(newInitials, numberCorrect) {
+    makeNewQuestionContainer();
+    var highScoresTitle = document.createElement("h1");
+    highScoresTitle.innerHTML = "Highscores";
+    newQuestion.appendChild(highScoresTitle);
+    highScoresList = document.createElement("div");
+    highScoresList.classList.add("text-center");
+    highScoresList.setAttribute("id", "highscores-list");
+    console.log(highScoresList);
+    var buttonDiv = document.createElement("div");
+    buttonDiv.classList.add("text-center");
+    var goBackButton = document.createElement("button");
+    goBackButton.setAttribute("id", "go-back");
+    goBackButton.innerHTML = "Go Back";
+    buttonDiv.appendChild(goBackButton);
+    var clearHighscoresButton = document.createElement("button");
+    clearHighscoresButton.setAttribute("id", "clear-highscores");
+    clearHighscoresButton.innerHTML = "Clear Highscores";
+    buttonDiv.appendChild(clearHighscoresButton);
+
+    placeholderDiv.appendChild(newQuestion);
+    var placeholderButtonDiv = document.getElementById("button-div");
+    placeholderButtonDiv.appendChild(buttonDiv);
+    highScoresExist = true;
+    addHighScore(newInitials, numberCorrect, highScoresList);
+}
+
+function addHighScore(newInitals, numberCorrect, highScoresList) {
+    var newScore = document.createElement("p");
+    newScore.innerHTML = newInitals + " " + numberCorrect + "\/" + numberTotalQuestions;
+    console.log(newScore);
+    console.log(highScoresList);
+    highScoresList.appendChild(newScore);
+    newQuestion.appendChild(highScoresList);
+    placeholderDiv.appendChild(newQuestion);
+}
+
 
 function addTwoBreaks(parentElement) {
     newBreak = document.createElement("br");
@@ -209,6 +286,7 @@ function nextQuestion() {
     } else if (currentQuestion === 3) {
         setUpQuestion4();
     } else {
+        stopTimer();
         displayEndScreen();
     }
 }
