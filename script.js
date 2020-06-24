3166b363303c117582705242e68e00259f8223e4
sbarrow825 <sbarrow825@berkeley.edu>
2020-06-23 19:04:11 -0700

    startup page now dissapears upon clicking start quiz button

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 var secondsLeft = document.querySelector("#seconds-left");
 var startButton = document.querySelector("#start-button");
-var interval;
+var startPage = document.querySelector("#start-page");
+// var introTextTitle = document.querySelector("#intro-text-title");
+// var startButtonText = document.querySelector("#start-button-text");
 
 secondsLeft.innerHTML = 75;
+// introTextTitle.innerHTML = "Coding Quiz Challenge";
+// startButton.innerHTML = "Start Quiz";
 
 startButton.addEventListener("click", startCountdown);
 
 function startCountdown() {
+    setUpQuiz();
     setInterval(tick, 1000);
 }
 
 function tick() {
     secondsLeft.innerHTML -= 1;
     console.log(secondsLeft.innerHTML);
+}
+
+function setUpQuiz() {
+    startPage.remove();
 }
