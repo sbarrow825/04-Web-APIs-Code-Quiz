4bba87ac0ab0d792a32c5541a06ca507b43d449f
sbarrow825 <sbarrow825@berkeley.edu>
2020-06-23 20:41:24 -0700

    console now correctly logs whether the chosen answer is correct or incorrect on the first quiz question

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ var secondsLeft = document.querySelector("#seconds-left");
 var startButton = document.querySelector("#start-button");
 var startPage = document.querySelector("#start-page");
 var placeholderDiv = document.querySelector("#place-holder");
-var question1Options = ["A string is a datatype", "A string can be mutated", "A string is denoted by quotation marks at the beginning and end", "A string can be saved to local storage on the browser"];
+var allButtons;
+var question1Options = ["2", "A string is a datatype", "A string can be mutated", "A string is denoted by quotation marks at the beginning and end", "A string can be saved to local storage on the browser"];
 var newQuestion;
 var newButton;
+var newButtons;
 var newBreak;
 // var introTextTitle = document.querySelector("#intro-text-title");
 // var startButtonText = document.querySelector("#start-button-text");
@@ -22,7 +24,6 @@ function startCountdown() {
 
 function tick() {
     secondsLeft.innerHTML -= 1;
-    console.log(secondsLeft.innerHTML);
 }
 
 function setUpQuiz() {
@@ -36,16 +37,23 @@ function setUpQuestion1() {
     question1Prompt.innerHTML = "Which of these is not true of a string";
     newQuestion.appendChild(question1Prompt);
     addTwoBreaks(newQuestion);
-    for (i = 0; i < question1Options.length; i += 1) {
+    for (i = 1; i < question1Options.length; i += 1) {
         newButton = document.createElement("button");
-        newButton.innerHTML = (i+1) + ") " + question1Options[i];
+        newButton.innerHTML = i + ") " + question1Options[i];
         newButton.classList.add("button");
         newButton.classList.add("answer-button");
+        if (parseInt(question1Options[0]) === i) {
+            newButton.setAttribute("data-correct", "true");
+        } else {
+            newButton.setAttribute("data-correct", "false");
+        }
+        // console.log(newButton.getAttribute("data-correct"));
         newButton.style.float = "left";
         newQuestion.appendChild(newButton);
         addTwoBreaks(newQuestion);
     }
     placeholderDiv.appendChild(newQuestion);
+    addNewEventListeners();
 }
 
 function addTwoBreaks(parentElement) {
@@ -57,4 +65,29 @@ function addTwoBreaks(parentElement) {
 
 function makeNewQuestionContainer() {
     newQuestion = document.createElement("card");
+}
+
+function addNewEventListeners() {
+    newButtons = document.getElementsByClassName("answer-button");
+    for (i = 0; i < newButtons.length; i += 1) {
+        newButtons[i].addEventListener("click", checkIfCorrect);
+    }
+}
+
+function checkIfCorrect() {
+    var correct = this.getAttribute("data-correct");
+    console.log(correct);
+    if (correct === "true") {
+        displayCorrect()
+    } else {
+        displayFalse()
+    }
+}
+
+function displayCorrect() {
+
+}
+
+function displayFalse() {
+
 }
