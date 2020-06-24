77c6bbb8b348fca8fe28ea57d5280a8c4b1ce5d9
sbarrow825 <sbarrow825@berkeley.edu>
2020-06-23 20:12:29 -0700

    first question questions now pop up upon clicking start quiz button

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ var secondsLeft = document.querySelector("#seconds-left");
 var startButton = document.querySelector("#start-button");
 var startPage = document.querySelector("#start-page");
 var placeholderDiv = document.querySelector("#place-holder");
+var question1Options = ["A string is a datatype", "A string can be mutated", "A string is denoted by quotation marks at the beginning and end", "A string can be saved to local storage on the browser"];
 var newQuestion;
+var newButton;
+var newBreak;
 // var introTextTitle = document.querySelector("#intro-text-title");
 // var startButtonText = document.querySelector("#start-button-text");
 
@@ -30,11 +33,28 @@ function setUpQuiz() {
 function setUpQuestion1() {
     makeNewQuestionContainer();
     var question1Prompt = document.createElement("h1");
-    question1Prompt.innerHTML = "This is a question";
+    question1Prompt.innerHTML = "Which of these is not true of a string";
     newQuestion.appendChild(question1Prompt);
+    addTwoBreaks(newQuestion);
+    for (i = 0; i < question1Options.length; i += 1) {
+        newButton = document.createElement("button");
+        newButton.innerHTML = (i+1) + ") " + question1Options[i];
+        newButton.classList.add("button");
+        newButton.classList.add("answer-button");
+        newButton.style.float = "left";
+        newQuestion.appendChild(newButton);
+        addTwoBreaks(newQuestion);
+    }
     placeholderDiv.appendChild(newQuestion);
 }
 
+function addTwoBreaks(parentElement) {
+    newBreak = document.createElement("br");
+    parentElement.appendChild(newBreak);
+    newBreak = document.createElement("br");
+    parentElement.appendChild(newBreak);
+}
+
 function makeNewQuestionContainer() {
-    newQuestion = document.createElement("div")
+    newQuestion = document.createElement("card");
 }
